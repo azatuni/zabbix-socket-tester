9245e02b476b6fb0bf4e4cb966dde5cdab3fe9cb
areg <areg@bravo.betconstruct.int>
2020-06-12 02:34:07 +0400

    First commit

---
+++ socket-tester.py
@@ -0,0 +1,23 @@
+import socket
+import sys
+
+default_timeout = 3
+address = sys.argv[1]
+port = int(sys.argv[2])
+
+try:
+ timeout = int(sys.argv[3])
+except IndexError:
+ timeout = int(default_timeout)
+
+sock = socket.socket()
+sock.settimeout(timeout)
+
+try:
+ sock.connect((address, port))
+ print(0)
+except Exception as e:
+ print(1)
+# print(f'something wrong with {address} {port}. Exception is {e}')
+finally:
+ sock.close()
