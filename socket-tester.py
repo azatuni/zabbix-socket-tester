3624946ccb739ae54d98d7c3d5c8ab177cc274d3
Areg Hakobyan <azatuni@gmail.com>
2020-12-15 03:50:26 +0400

    Ref pep8

--- socket-tester.py
+++ socket-tester.py
@@ -6,18 +6,18 @@ address = sys.argv[1]
 port = int(sys.argv[2])
 
 try:
- timeout = int(sys.argv[3])
+    timeout = int(sys.argv[3])
 except IndexError:
- timeout = int(default_timeout)
+    timeout = int(default_timeout)
 
 sock = socket.socket()
 sock.settimeout(timeout)
 
 try:
- sock.connect((address, port))
- print(0)
+    sock.connect((address, port))
+    print(0)
 except Exception as e:
- print(1)
+    print(1)
 # print(f'something wrong with {address} {port}. Exception is {e}')
 finally:
- sock.close()
+    sock.close()
